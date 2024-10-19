b603404e67be5d0f35735524958a915d3b8d491f
ttserafim1 <163613745+ttserafim1@users.noreply.github.com>
2024-10-19 20:47:13 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
 document.getElementById('registerForm').addEventListener('submit', function(event) {
 event.preventDefault();
+const email = document.getElementById('email').value;
+
+emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", {
+to_email: email
+})
+.then(function(response) {
+alert('Письмо отправлено! Проверьте вашу почту.');
+}, function(error) {
+console.error('Ошибка при отправке письма:', error);
+alert('Произошла ошибка при отправке письма.');
+});
+
 window.location.href = 'https://ttserafim1.github.io/login-/';
 });
 
